89219bf586dca597512fbde9f6bcc5b51856dc63
Rebecca Swernofsky <rswernofsky@gmail.com>
2020-04-30 13:52:21 -0400

    add peanut butter pairings list

--- projects/snake.js
+++ projects/snake.js
@@ -5,43 +5,47 @@ let canvas, context, frames;
 const size = 20;
 const gridSize = 15;
 
-// represents a single grid square
-class GridSquare {
-    constructor(x, y, color) {
+// represents a coordinate in terms of grid coordinates, not pixel coordinates
+class Coord {
+    constructor(x, y) {
         this.x = x;
         this.y = y;
-        this.color = color;
     }
 
-    // is the given grid square at the same coordinate as this one?
+    // is the given coordinate the same as this one?
     equals(square) {
         return (square.x === this.x && square.y === this.y);
     }
 
-    // draws this grid square
+    // draws this coordinate (with the context's current color) using the game's
+    // constant square size
     draw() {
         context.fillRect(this.x * size, this.y * size, size, size);
     }
 
-    // 
+    // generates a new coordinate in a random location
     static random() { // lowercase is good notation 
-        return new GridSquare(getRandomInt(gridSize),
+        return new Coord(getRandomInt(gridSize),
             getRandomInt(gridSize));
     }
 }
-// each grid square will make a square that has a width and height of 20
 
+// generates a random integer between 0 and the given integer
 const getRandomInt = (upTo) => Math.floor(upTo * Math.random());
+
+// generates a random color
 const getRandomColor = () => '#' + Math.floor((1 << 24) * Math.random()).toString(16);
 
-let snake = [GridSquare.random()];
-// list of the "coordinates" of the squares which will make up the snake shape. Will get multiplied to find the x and y locations
-let fruit = new GridSquare(1, 1);
-let gameover = false;
+// the snake is an array, and it's initialized with just one segment
+let snake = [Coord.random()];
+let fruit = new Coord(1, 1); // initialize the fruit to be at grid coordinates (1, 1)
+let gameover = false; // initialize the game to be in play
 
+// ticks the game
 const loop = () => {
     // window.requestAnimationFrame(loop); //runs every animation frame
 
+    // draws the background, snake, and fruit
     context.fillStyle = '#000000';
     context.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -54,53 +58,56 @@ const loop = () => {
     for (let i = 1; i < snake.length; i++) {
         snake[i].draw();
     }
-    // draws the snake and the fruit
 
-    let head = new GridSquare(snake[0].x, snake[0].y);
+    let head = new Coord(snake[0].x, snake[0].y);
     lastDir = direction;
 
+    // changes the next head according to the direction
+    // if direction is undefined, nothing happens
     switch (direction) {
         case 1: head.y--; break; // up
         case 3: head.y++; break; // down
         case 0: head.x--; break; // left
         case 2: head.x++; break; // right
     }
-    // changes the next head according to the direction
 
+    // warp the head onto the other side if it goes off the grid
     if (head.x >= gridSize) head.x = 0;
     if (head.x < 0) head.x = gridSize;
     if (head.y >= gridSize) head.y = 0;
     if (head.y < 0) head.y = gridSize;
-    // loop onto the other side if head goes off the grid
 
+    // checks whether the snake is running into itself
     for (i = 1; i < snake.length; i++) {
         if (head.x === snake[i].x && head.y === snake[i].y) {
             gameover = true;
             clearInterval(refreshIntervalId);
         }
     }
-    // checks if the snake is running into itself
 
+    // move the whole snake 1 grid square
     shiftUp();
     snake[0] = head;
 
+    // when the head runs into fruit
     if (head.equals(fruit)) {
         snake[snake.length] = snake[snake.length - 1];
         // will return true if any of the pieces of the snake are the same as the fruit
         while (snake.some((piece) => piece.equals(fruit))) {
-            fruit = GridSquare.random();
+            fruit = Coord.random();
         }
     }
-    // when the head runs into fruit
-
 };
 
+// replace every segment's coordinate in the snake (except for the head) with the coordinate
+// before it
 const shiftUp = () => {
     for (let i = snake.length - 2; i >= 0; i--) {
         snake[i + 1] = snake[i];
     }
 };
 
+// a strategic mapping of keys
 const keyMap = {
     38: 1,  // up
     40: 3,  // down
@@ -108,21 +115,24 @@ const keyMap = {
     39: 2   // right
 };
 
-let direction = undefined; // not a real direction
+let direction = undefined; // don't start with any particular direction
 let lastDir = direction;
 
 document.onkeydown = (e) => {
-    if (e.keyCode === 32) stopAnimation();
-    const newDir = keyMap[e.keyCode];
+    if (e.keyCode === 32) toggleAnimation(); // press space to pause the game
+    const newDir = keyMap[e.keyCode]; // keyMap of a key that's not an arrow key will be undefined
+    // if the new direction is directly opposite the current one, or a non-arrow key was pressed
+    // don't update the direction
     if (isOppositeDir(lastDir, newDir) ||
         newDir === undefined) return;
     direction = newDir;
-    // if the keyMap is defined, it will return it without looking at direction
 };
 
+// are the two given directions direct opposites of each other?
 const isOppositeDir = (dir1, dir2) => (dir1 + dir2) % 2 === 0;
 
-const stopAnimation = () => {
+// toggle whether the game is being played
+const toggleAnimation = () => {
     if (animating || gameover) clearInterval(refreshIntervalId);
     else refreshIntervalId = setInterval(loop, 1000 / fps);
     animating = !animating;
@@ -132,6 +142,7 @@ let refreshIntervalId = 0;
 let animating = true;
 const fps = 5;
 
+// canvas setup
 window.onload = () => {
     canvas = $('#canvas')[0];
     context = canvas.getContext('2d');
